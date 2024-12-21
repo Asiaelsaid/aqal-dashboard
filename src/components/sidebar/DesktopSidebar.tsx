fabc5bacbe295b712eebc608f759869859e4007d
Asia Magdy <asiaelsaid665@gmail.com>
2024-12-21 13:41:04 +0200

    Implement : responsive sidebar with toggle functionality

---
+++ src/components/sidebar/DesktopSidebar.tsx
@@ -0,0 +1,98 @@
+import { FiLayers, FiPieChart, FiSearch, FiSettings } from "react-icons/fi";
+import Logo from "@assets/images/Logo.png"
+import { BsClipboard2Data, BsDatabaseCheck } from "react-icons/bs";
+import { BiSolidSelectMultiple } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { CgSupport } from "react-icons/cg";
+
+interface IProps {
+    isOpen: boolean;
+    handleToggle: () => void;
+    activeItem: string;
+    setActiveItem: (item: string) => void;
+}
+
+const DesktopSidebar: React.FC<IProps> = ({isOpen,handleToggle ,activeItem,setActiveItem}) => {
+    return <div
+    className={` hidden lg:flex lg:flex-col fixed  px-6 py-8 bg-mainColor text-textColor ${
+      isOpen ? "w-64" : "w-20"
+    } transition-width duration-500 h-screen fixed ease-in-out flex flex-col`}
+  >
+    <div
+      className="flex items-center justify-center text-l font-bold cursor-pointer "
+      onClick={handleToggle}
+    >
+      <img src={Logo} alt="header logo" className="mr-2" />
+      <span className={` ${isOpen ? "" : "hidden"}`}>Aqal Management</span>
+    </div>
+
+    <div className={`mt-6  ${isOpen ? "" : "hidden"}`}>
+      <div className="flex items-center bg-hoverColor text-white text-opacity-70 rounded-lg px-4 py-2  shadow-md space-x-2 mt-3">
+        <FiSearch className="text-white text-opacity-70 mr-2" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-transparent outline-none text-white text-opacity-70 placeholder-white placeholder-opacity-70 w-full"
+        />
+      </div>
+    </div>
+
+    <div className="mt-6 space-y-2 flex-grow">
+      {[
+        { label: "Dashboard", icon: <BsClipboard2Data />, path: "/" },
+        { label: "Properties", icon: <FiLayers />, path: "properties" },
+        {
+          label: "Tenants",
+          icon: <BiSolidSelectMultiple />,
+          path: "tenants",
+        },
+        {
+          label: "Financials",
+          icon: <BsDatabaseCheck />,
+          path: "financials",
+        },
+        { label: "Reporting", icon: <FiPieChart />, path: "reporting" },
+      ].map((item) => (
+        <Link
+          to={item.path}
+          key={item.label}
+          onClick={() => setActiveItem(item.label)}
+          className={`flex items-center p-2 rounded-lg cursor-pointer hover:bg-hoverColor ${
+            activeItem === item.label ? "bg-hoverColor" : ""
+          }`}
+        >
+          <span className="text-xl mr-3">{item.icon}</span>
+          <span
+            className={`text-sm font-medium p-2 ${isOpen ? "" : "hidden"}`}
+          >
+            {item.label}
+          </span>
+        </Link>
+      ))}
+    </div>
+    <div className=" space-y-4">
+      {[
+        { label: "Support", icon: <CgSupport />, path: "support" },
+        { label: "Settings", icon: <FiSettings />, path: "settings" },
+      ].map((item) => (
+        <Link
+          to={item.path}
+          key={item.label}
+          onClick={() => setActiveItem(item.label)}
+          className={`flex items-center p-2 rounded-lg cursor-pointer hover:bg-hoverColor ${
+            activeItem === item.label ? "bg-hoverColor" : ""
+          }`}
+        >
+          <span className="text-xl mr-3">{item.icon}</span>
+          <span
+            className={`text-sm font-medium p-2 ${isOpen ? "" : "hidden"}`}
+          >
+            {item.label}
+          </span>
+        </Link>
+      ))}
+    </div>
+  </div>
+};
+
+export default DesktopSidebar;
---
+++ src/components/sidebar/MobileSidebar.tsx
@@ -0,0 +1,97 @@
+import { FiLayers, FiPieChart, FiSettings } from "react-icons/fi";
+import Logo from "@assets/images/Logo.png";
+import { BsClipboard2Data, BsDatabaseCheck } from "react-icons/bs";
+import { BiSolidSelectMultiple } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { CgSupport } from "react-icons/cg";
+
+interface IProps {
+  isOpen: boolean;
+  handleToggle: () => void;
+  activeItem: string;
+  setActiveItem: (item: string) => void;
+}
+
+const MobileSidebar: React.FC<IProps> = ({
+  isOpen,
+  handleToggle,
+  activeItem,
+  setActiveItem,
+}) => {
+  return (
+    <div
+      className={`lg:hidden fixed top-0 left-0 h-full text-textColor ${
+        isOpen ? "bg-mainColor w-64" : "bg-transparent w-20"
+      } transition-all duration-300 ease-in-out z-50 flex flex-col`}
+    >
+
+      <div className="flex justify-between items-center p-4">
+        <button className= {`${isOpen ?"text-white text-2xl" :"text-purple-500 text-2xl" }`} onClick={handleToggle}>
+          ☰
+        </button>
+      </div>
+
+      {/* Sidebar Content (Visible only when isOpen is true) */}
+      {isOpen && (
+        <>
+          <div className="flex justify-center p-4">
+            <img src={Logo} alt="header logo" className="w-10 h-10" />
+          </div>
+
+          {/* Main Navigation Links */}
+          <div className="flex flex-col space-y-4 mt-6">
+            {[{ label: "Dashboard", icon: <BsClipboard2Data />, path: "/" },
+              { label: "Properties", icon: <FiLayers />, path: "properties" },
+              {
+                label: "Tenants",
+                icon: <BiSolidSelectMultiple />,
+                path: "tenants",
+              },
+              {
+                label: "Financials",
+                icon: <BsDatabaseCheck />,
+                path: "financials",
+              },
+              { label: "Reporting", icon: <FiPieChart />, path: "reporting" },
+            ].map((item) => (
+              <Link
+                to={item.path}
+                key={item.label}
+                onClick={() => {setActiveItem(item.label)
+                  handleToggle();
+                }}
+                className={`flex items-center p-2 rounded-lg cursor-pointer hover:bg-hoverColor ${
+                  activeItem === item.label ? "bg-hoverColor" : ""
+                }`}
+              >
+                <span className="text-xl mr-3">{item.icon}</span>
+                <span className="text-sm font-medium">{item.label}</span>
+              </Link>
+            ))}
+          </div>
+
+          {/* Additional Links (Support, Settings) */}
+          <div className="flex flex-col space-y-4 mt-auto">
+            {[{ label: "Support", icon: <CgSupport />, path: "support" },
+              { label: "Settings", icon: <FiSettings />, path: "settings" },
+            ].map((item) => (
+              <Link
+                to={item.path}
+                key={item.label}
+                onClick={() => setActiveItem(item.label)}
+                className={`flex items-center p-2 rounded-lg cursor-pointer hover:bg-hoverColor ${
+                  activeItem === item.label ? "bg-hoverColor" : ""
+                }`}
+              >
+                <span className="text-xl mr-3">{item.icon}</span>
+                <span className="text-sm font-medium">{item.label}</span>
+              </Link>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MobileSidebar;
