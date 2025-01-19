bad2624238356e7de90827bf923e916d4fe53ee4
Asia Magdy <asiaelsaid665@gmail.com>
2025-01-19 19:27:42 +0200

    handle :just admin can be add a new property

--- src/components/Properties/PropertiesFilter.tsx
+++ src/components/Properties/PropertiesFilter.tsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddPropertyModal from "./AddProperty/AddPropertyModal";
 import SelectCountry from "./Filters/SelectCountry";
 import SubHeading from "@components/UI/SubHeading";
 import { FaPlus } from "react-icons/fa6";
-
 
 const PropertiesFilter = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,7 +11,7 @@ const PropertiesFilter = () => {
     imgURL: "",
   });
   // const [selectedPriceRange, setSelectedPriceRange] = useState(null);
-console.log(selectedCountry);
+  console.log(selectedCountry);
 
   // Handlers
   const onCountryChange = (category: { name: string; imgURL: string }) => {
@@ -26,10 +25,14 @@ console.log(selectedCountry);
   //   setSelectedPriceRange(priceRange);
   // };
   const openModal = () => setIsOpen(true);
-
+  const [role, setRole] = useState<string>("");
+  useEffect(() => {
+    const storedRole: string = localStorage.getItem("role") as string;
+    setRole(storedRole);
+  }, []);
   return (
     <div className="space-y-6">
-      <SubHeading subHeading="Manage all your properties in one place"/>
+      <SubHeading subHeading="Manage all your properties in one place" />
       {/* Filters */}
       <div className="flex flex-col sm:flex-row sm:space-x-4 sm:space-y-0 space-y-4 sm:justify-between lg:flex-row lg:items-center lg:space-x-8">
         {/* Dropdowns */}
@@ -40,14 +43,16 @@ console.log(selectedCountry);
         </div>
 
         {/* Button */}
-        <div className="flex justify-center sm:justify-end w-full sm:w-auto">
-          <button
-            onClick={openModal}
-            className="px-6 py-3 flex items-center text-sm font-medium text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-700"
-          >
-             <FaPlus className="mr-2"/> Add new property
-          </button>
-        </div>
+        {role === "admin" && (
+          <div className="flex justify-center sm:justify-end w-full sm:w-auto">
+            <button
+              onClick={openModal}
+              className="px-6 py-3 flex items-center text-sm font-medium text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-700"
+            >
+              <FaPlus className="mr-2" /> Add new property
+            </button>
+          </div>
+        )}
       </div>
       <AddPropertyModal isOpen={isOpen} setIsOpen={setIsOpen} />
     </div>
