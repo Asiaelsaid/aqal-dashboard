b4bef8527e44a58db2f8e9582f2631fd46141113
Asia Magdy <asiaelsaid665@gmail.com>
2025-02-28 08:45:07 +0200

    remove filter buttons from units table

--- src/components/Properties/PropertyDetails/units/UnitsTable.tsx
+++ src/components/Properties/PropertyDetails/units/UnitsTable.tsx
@@ -58,7 +58,7 @@ const UnitsTable: React.FC<UnitsTableProps> = ({
           Access the Access the inspection Management sheet
         </p>
 
-        <div className="mt-4 inline-flex rounded-md shadow-sm" role="group">
+        {/* <div className="mt-4 inline-flex rounded-md shadow-sm" role="group">
           <button className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-l-md hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-purple-500 focus:text-purple-500">
             Sold units
           </button>
@@ -68,7 +68,7 @@ const UnitsTable: React.FC<UnitsTableProps> = ({
           <button className="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-r-md hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-purple-500 focus:text-purple-500">
             Vacant units
           </button>
-        </div>
+        </div> */}
 
         <h2 className="text-lg font-medium mt-6">
           Tenants list( {properyName})
